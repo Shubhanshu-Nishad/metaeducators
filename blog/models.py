d72d37072d076ed959165f75da4d8f8f0c1495d4
Shubhanshu <shubhanshunishad2221@gmail.com>
2020-08-11 19:14:53 +0530

    I have complete the blog post part but i have to correct to slug

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    sno = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    slug=models.CharField(max_length=155)
+    timeStamp=models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return self.title +'by'+ self.author
