b18b90086024e70cfa2cac1cce7f81b95eaa2b9e
Shubhanshu <shubhanshunishad2221@gmail.com>
2020-09-01 08:56:28 +0530

    it is ready to merge

--- metaeducator/settings.py
+++ metaeducator/settings.py
@@ -90,19 +90,14 @@ WSGI_APPLICATION = 'metaeducator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'meta',
-        'USER': 'root',
-        'PORT': '3306',
-        'PASSWORD': '',
-        'HOST': 'localhost'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
